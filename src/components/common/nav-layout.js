7fb28c3232d2087b54f8a513c0a4127af28c1ee3
Krushi Raj Tula <krushiraj123@gmail.com>
2019-11-12 23:39:10 +0530

    Update nav-layout.js
    Updated tab names and paths. Changed colors to suit the site theme

--- src/components/common/nav-layout.js
+++ src/components/common/nav-layout.js
@@ -3,7 +3,7 @@ import React from 'react'
 import "../../styles/my-styles.css"
 
 const GragittyNavLogo = () => (
-  <div className="flex-1 flex justify-between items-center">
+  <div className="flex-1 flex items-center">
     <a href="#">
       <svg
         width="32"
@@ -14,6 +14,7 @@ const GragittyNavLogo = () => (
         <path d="M15.922 35.798c-.946 0-1.852-.228-2.549-.638l-10.825-6.379c-1.428-.843-2.549-2.82-2.549-4.501v-12.762c0-1.681 1.12-3.663 2.549-4.501l10.825-6.379c.696-.41 1.602-.638 2.549-.638.946 0 1.852.228 2.549.638l10.825 6.379c1.428.843 2.549 2.82 2.549 4.501v12.762c0 1.681-1.12 3.663-2.549 4.501l-10.825 6.379c-.696.41-1.602.638-2.549.638zm0-33.474c-.545 0-1.058.118-1.406.323l-10.825 6.383c-.737.433-1.406 1.617-1.406 2.488v12.762c0 .866.67 2.05 1.406 2.488l10.825 6.379c.348.205.862.323 1.406.323.545 0 1.058-.118 1.406-.323l10.825-6.383c.737-.433 1.406-1.617 1.406-2.488v-12.757c0-.866-.67-2.05-1.406-2.488l-10.825-6.379c-.348-.21-.862-.328-1.406-.328zM26.024 13.104l-7.205 13.258-3.053-5.777-3.071 5.777-7.187-13.258h4.343l2.803 5.189 3.107-5.832 3.089 5.832 2.821-5.189h4.352z"></path>
       </svg>
     </a>
+    <span className="mx-4 font-semibold text-2xl">Gragitty</span>
   </div>
 );
 
@@ -41,44 +42,44 @@ const NavMenuItems = () => (
       <ul className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
         <li>
           <a
-            className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400"
-            href="#"
+            className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-green-400"
+            href="/"
           >
-            Features
+            Home
           </a>
         </li>
         <li>
           <a
-            className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400"
-            href="#"
+            className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-green-400"
+            href="/calendar"
           >
-            Pricing
+            Calendar
           </a>
         </li>
         <li>
           <a
-            className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400"
-            href="#"
+            className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-green-400"
+            href="/tasks"
           >
-            Documentation
+            Tasks
           </a>
         </li>
         <li>
           <a
-            className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400 lg:mb-0 mb-2"
-            href="#"
+            className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-green-400 lg:mb-0 mb-2"
+            href="/about"
           >
-            Support
+            About
           </a>
         </li>
       </ul>
     </nav>
     <a
-      href="#"
+      href="/profile"
       className="lg:ml-4 flex items-center justify-start lg:mb-0 mb-4 pointer-cursor"
     >
       <img
-        className="m-auto rounded-full w-10 h-10 border-2 border-transparent hover:border-indigo-400"
+        className="m-auto rounded-full w-10 h-10 border-2 border-transparent hover:border-green-400"
         src="https://pbs.twimg.com/profile_images/1128143121475342337/e8tkhRaz_normal.jpg"
         alt="Andy Leverenz"
       />
